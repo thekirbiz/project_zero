fac7de578c5f1a2a7fe2d54528aa8df8262c9209
thekirbiz <kirbiz111@outlook.com>
2026-03-22 16:05:43 +0300

    clean project

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: '/project_zero',
-    assetPrefix: '/project_zero/',
-}
-
-module.exports = nextConfig
